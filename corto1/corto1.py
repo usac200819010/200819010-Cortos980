fd61052cf96f05e12147971f84d53580d0637d84
200819010RolandoEstuardoSubuyujJuarez <2558610600101@ingenieria.usac.edu.gt>
2020-06-09 12:34:06 -0600

    primer push

---
+++ corto1/corto1.py
@@ -0,0 +1,49 @@
+print('''
+      
+     
+    si el número es par, se lo divide por dos;
+    si es impar, se le multiplica tres y se le suma uno;
+    la sucesión termina al llegar a uno.
+ 
+      
+      ''')
+
+
+#carnet 200819010
+
+inicio = 11
+
+modulo = 0
+valor = True
+sig = 0
+lista = []
+while valor :
+        #del lista[:]
+        for i in range(2, inicio+1):
+            modulo = i%2 #si el modulo es 0 es par si es 1 es impar
+            
+            if inicio == 11:
+                valor = False        
+
+            if modulo == 1:
+                sig = int(3*i+1)
+                #print(i,sig, "impar")
+                inicio = sig
+                lista.append(sig)
+            else:
+                sig = int(i/2)
+                #print(sig)
+                inicio = sig
+                #print(i,sig, "par")
+                lista.append(sig)   
+            inicio = lista[-1]     
+            print(i, lista[-1], inicio,lista)
+            
+        archivo = open('collatz.txt', 'w')
+        
+                
+
+
+        
+#print(lista, ": ", len(lista) )
+#print("la secuencia Collatz está dada por: ",lista )
